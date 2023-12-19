737e01055bd0a515af1af4e6d20d6294eb8e09e9
cuiyue <cuiyue.msn@hotmail.com>
2023-12-19 15:05:20 +0800

    雷达根据alarmType来显示单机与风络版

--- src/component/map/radar-info/radar-info.tsx
+++ src/component/map/radar-info/radar-info.tsx
@@ -5,10 +5,14 @@ import CloseCircleOutlined from '@ant-design/icons/CloseCircleOutlined';
 import { Button } from 'antd';
 import { helper } from '@/utility/helper'
 import { Protocol } from '@/schema/protocol';
+import { AlarmType } from '@/schema/conf';
+import { AlarmMessage } from '@/schema/phone-alarm-info';
 import { Point } from './point';
 import { pointMap, getLoopIndex } from './rnd-point';
 import { RadarBox } from './styled/box';
 import { PointAt, RadarInfoProp } from './prop';
+
+const { alarmType } = helper.readConf();
 
 let timer: any = null;
 
@@ -36,64 +40,87 @@ const RadarInfo: FC<RadarInfoProp> = ({ open, data, deviceId, onClose }) => {
     }, [open]);
 
     const renderPoint = () => {
-        if (deviceId === undefined || data[deviceId] === undefined) {
-            return null;
+
+        let alarms: AlarmMessage[] = [];
+
+        if (alarmType === AlarmType.Single) {
+            //单机版
+            for (let [, v] of Object.entries(data)) {
+                alarms = alarms.concat(v.filter(item => {
+                    //过滤掉超过5秒的点
+                    const isOver = dayjs().diff(item.captureTime, 's') <= 5;
+                    return isOver;
+                }));
+            }
+        } else {
+            //网络版（筛选当前deviceId的报警）
+            if (deviceId === undefined || data[deviceId] === undefined) {
+                return null;
+            } else {
+                alarms = alarms.concat(data[deviceId]
+                    .filter(item => {
+                        //过滤掉超过5秒的点
+                        const isOver = dayjs().diff(item.captureTime, 's') <= 5;
+                        return isOver;
+                    }));
+            }
         }
 
-        return data[deviceId]
-            .filter(item => {
-                //过滤掉超过5秒的点
-                const isOver = dayjs().diff(item.captureTime, 's') <= 5;
-                return isOver;
-            })
-            .map((item, index) => {
-                const loop = getLoopIndex(Number(item.rssi!)); //信号强度所在环数
-                const points = pointMap.get(loop)!;//该环上所有的点，随机取
-                let top = 0, left = 0;
-                if (m.current.has(item.protocolType!)) {
-                    //如果已有该协议的数据，且环数相同，则不更换top&left值
-                    const p = m.current.get(item.protocolType!)!;
-                    if (p?.loop === loop) {
-                        // console.log('环数相同', p.loop, loop);
-                        top = p.top;
-                        left = p.left;
-                    } else {
-                        // console.log('环数不同', p.loop, loop);
-                        top = points[helper.rnd(0, points.length)].top;
-                        left = points[helper.rnd(0, points.length)].left;
-                        m.current.set(item.protocolType!, { top, left, loop });
-                    }
-
+        return alarms.map((item, index) => {
+            const loop = getLoopIndex(Number(item.rssi!)); //信号强度所在环数
+            const points = pointMap.get(loop)!;//该环上所有的点，随机取
+            let top = 0, left = 0;
+            if (m.current.has(item.protocolType!)) {
+                //如果已有该协议的数据，且环数相同，则不更换top&left值
+                const p = m.current.get(item.protocolType!)!;
+                if (p?.loop === loop) {
+                    // console.log('环数相同', p.loop, loop);
+                    top = p.top;
+                    left = p.left;
                 } else {
+                    // console.log('环数不同', p.loop, loop);
                     top = points[helper.rnd(0, points.length)].top;
                     left = points[helper.rnd(0, points.length)].left;
-                    // console.log(`强度：${Number(item.rssi!)},环数：${loop}, 点：${top}-${left}`);
                     m.current.set(item.protocolType!, { top, left, loop });
                 }
 
-                return <Point
-                    // visible={dayjs().diff(item.captureTime, 's') <= 5}
-                    top={top}
-                    left={left}
-                    data={item}
-                    key={`P_${index}`} />;
-            });
+            } else {
+                top = points[helper.rnd(0, points.length)].top;
+                left = points[helper.rnd(0, points.length)].left;
+                // console.log(`强度：${Number(item.rssi!)},环数：${loop}, 点：${top}-${left}`);
+                m.current.set(item.protocolType!, { top, left, loop });
+            }
+
+            return <Point
+                // visible={dayjs().diff(item.captureTime, 's') <= 5}
+                top={top}
+                left={left}
+                data={item}
+                key={`P_${index}`} />;
+        });
     };
 
     const points = renderPoint();
 
     const renderInfo = () => {
-        if (deviceId === undefined || data[deviceId] === undefined) {
-            return null;
+
+        let alarms: AlarmMessage[] = [];
+        if (alarmType === AlarmType.Single) {
+            //单机版
+            alarms = Object.values(data).flat();
+        } else {
+            //网络版（筛选当前deviceId的最新报警）
+            if (deviceId === undefined || data[deviceId] === undefined) {
+                return null;
+            }
+            if (points === null || points.length === 0) {
+                return null;
+            }
+            alarms = data[deviceId];
         }
 
-        if (points === null || points.length === 0) {
-            return null;
-        }
-
-        const next = data[deviceId];
-        if (next.length > 0) {
-            const msg = maxBy(next, (item) => item.captureTime);
+        if (alarms.length > 0) {
+            const msg = maxBy(alarms, (item) => item.captureTime);
             // const msg = next[next.length - 1];
 
             return <div className="adetail">
@@ -141,7 +168,7 @@ const RadarInfo: FC<RadarInfoProp> = ({ open, data, deviceId, onClose }) => {
             {renderInfo()}
         </div>
         {
-            data === undefined
+            alarmType === AlarmType.Single
                 ? null
                 : <Button
                     onClick={() => onClose!()}
