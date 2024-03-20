09cdfa074b55e2b32f32ed3069823def1f1fcdc9
cuiyue <cuiyue.msn@hotmail.com>
2024-03-20 13:50:33 +0800

    修改文字

--- src/component/alarm/alarm-desc.tsx
+++ src/component/alarm/alarm-desc.tsx
@@ -13,7 +13,7 @@ const AlarmDesc: FC<{ data: AlarmMsg }> = ({ data }) => {
         <Item label="类型" span={3}>{data?.protocol}</Item>
         <Item label="告警级别" span={3}>{data?.warnLevel ?? '---'}</Item>
         <Item label="告警原因" span={3}>{data?.warnReason ?? '---'}</Item>
-        <Item label="点信息名称" span={3}>{data?.arfcn ?? '---'}</Item>
+        <Item label="频率信息名称" span={3}>{data?.arfcn ?? '---'}</Item>
         <Item label="强度值" span={3}>{data?.rssi ?? '---'}</Item>
         <Item label="设备ID" span={3}>{data?.deviceId ?? '---'}</Item>
         <Item label="设备场所" span={3}>{data?.siteName ?? '---'}</Item>
