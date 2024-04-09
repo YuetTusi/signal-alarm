9e09ff54cdd61e3647d7a29e4f4b6cb8331f3e3c
cuiyue <cuiyue.msn@hotmail.com>
2024-04-09 10:40:03 +0800

    频段改为显示类型名称

--- src/component/statis/signal-list.tsx
+++ src/component/statis/signal-list.tsx
@@ -9,6 +9,7 @@ import { DisplayPanel } from '../panel';
 import { ScrollList } from './styled/box';
 
 var scrollTimer: any = null;
+var bands = helper.readBand();
 
 /**
  * 可疑持续信号列表
@@ -54,6 +55,8 @@ const SignalList: FC<{}> = () => {
         }
     }, []);
 
+    useEffect(() => { console.log(signalData) }, [signalData]);
+
     /**
      * 执行滚动
      */
@@ -98,16 +101,32 @@ const SignalList: FC<{}> = () => {
     };
 
     const renderItem = () =>
-        signalData.map(item =>
-            <li
+        signalData.map(item => {
+            const band = bands.find(i => i.code === Number.parseInt(item.freqBand));
+            let txt = '';
+            if (band === undefined) {
+                txt = '其他';
+            } else {
+                if (band.name === '移动/广电(4G-B41/5G-N41)') {
+                    txt = 'B41/N41';
+                }
+                else if (band.name.includes('-')) {
+                    const from = band.name.lastIndexOf('-');
+                    const to = band.name.lastIndexOf(')');
+                    txt = band.name.substring(from + 1, to);
+                } else {
+                    txt = band.name;
+                }
+            }
+            return <li
                 onClick={() => onSignalItemClick(item)}
                 className="signal-li"
                 key={`SI_${item.id}`}>
-                <Tag color="blue">频段:{item.freqBand}</Tag>
+                <Tag color="blue" style={{ textAlign: 'center' }}>{txt}</Tag>
                 <Tag color="green">最新频率:{item.lastFreq}</Tag>
                 <Tag color="cyan">持续时间:{item.duration}s</Tag>
             </li>
-        );
+        });
 
     return <DisplayPanel>
         <div className="caption">最新可疑持续信号</div>
